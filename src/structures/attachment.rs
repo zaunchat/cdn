19a73fb255b967188c1be68b28a24243df059999
Abdulrahman Salah <abdulrahman.8alah@gmail.com>
2022-07-13 15:28:13 +0000

    fix: serialize bigint as string

--- src/structures/attachment.rs
+++ src/structures/attachment.rs
@@ -1,6 +1,6 @@
-use ormlite::model::*;
-use ormlite::types::Json;
-use serde::{Deserialize, Serialize};
+use ormlite::{model::*, types::Json};
+use serde::{Deserialize, Serialize, Serializer};
+use std::fmt::Display;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
@@ -15,10 +15,19 @@ pub enum Metadata {
 #[derive(Model, FromRow, Serialize, Debug)]
 #[ormlite(table = "attachments")]
 pub struct Attachment {
+    #[serde(serialize_with = "bigint_to_string")]
     pub id: i64,
     pub name: String,
     pub meta: Json<Metadata>,
     pub content_type: String,
     pub size: i32,
     pub tag: String,
+}
+
+pub fn bigint_to_string<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: Display,
+    S: Serializer,
+{
+    serializer.collect_str(value)
 }
